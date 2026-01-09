963346b35561a238da61573ef2dcad95f0cfbd41
Vladimir Pérez <vladimirck@gmail.com>
2026-01-09 00:17:54 -0400

    feat: implement chirp validation endpoint with error handling

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,43 @@ app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get(API + "/healthz",handleReadiness);
 app.get(ADMIN + "/metrics", handleHitsCounter);
 app.post(ADMIN + "/reset", handleResetCounter);
+app.post(API + "/validate_chirp", handleValidateChirp);
+
+function handleValidateChirp(req: Request, res: Response) {
+    var reqBody = '';
+    req.on('data', (chunk) => {
+        reqBody += chunk;
+    });
+
+    req.on('end', () => {
+        var chirp: {body:string, error?:string};
+        try{
+            chirp = JSON.parse(reqBody);
+        } catch (e) {
+            res.set("Content-Type", "application/json");
+            res.status(400).send(JSON.stringify({ error: "Something went wrong" }));
+            return;
+        }
+        
+        if (!chirp.body){
+            res.set("Content-Type", "application/json");
+            res.status(400).send(JSON.stringify({ error: "Something went wrong" }));
+            return;
+        }
+
+        if (chirp.body.length > 140) {
+            res.set("Content-Type", "application/json");
+            res
+              .status(400)
+              .send(JSON.stringify({ error: "Chirp is too long" }));
+            return;
+        }
+        
+        res.set("Content-Type", "application/json");
+        res.status(200).send(JSON.stringify({ valid: true }));
+    });
+    
+}
 
 function handleReadiness(req: Request, res: Response) {
     res.set("Content-Type", "text/plain");
